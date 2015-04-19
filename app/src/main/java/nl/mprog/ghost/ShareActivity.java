48374882b57aaa87ccf9c474ed2f873a9d0d165a
gitjoop <joop.pascha@student.uva.nl>
2015-04-19 21:30:28 +0200

    ok

--- app/src/main/java/nl/mprog/ghost/ShareActivity.java
+++ app/src/main/java/nl/mprog/ghost/ShareActivity.java
@@ -6,20 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-
-public class MainActivity extends ActionBarActivity {
+public class ShareActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_share);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_share, menu);
         return true;
     }
 
--- app/src/main/java/nl/mprog/ghost/UserSettingsActivity.java
+++ app/src/main/java/nl/mprog/ghost/UserSettingsActivity.java
@@ -6,20 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-
-public class MainActivity extends ActionBarActivity {
+public class UserSettingsActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_user_settings);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_user_settings, menu);
         return true;
     }
 
